130709f5f0db2f388ca6db225d618ef01b30c51d
KevinFroissart <kevin.froissart69@gmail.com>
2021-11-09 20:34:15 +0100

    candidat service

---
+++ src/main/java/fr/univlyon1/m1if/m1if03/classes/services/CandidatsService.java
@@ -0,0 +1,21 @@
+package fr.univlyon1.m1if.m1if03.classes.services;
+
+public class CandidatsService  {
+
+    public static void getCandidats() {
+        // TODO: implémenter
+    }
+
+    public static void getNomCandidats() {
+        // TODO: implémenter
+    }
+
+    public static void getCandidat(int idCandidat) {
+        // TODO: implémenter
+    }
+
+    public static void updateCandidats(/*List<Candidats> candidats*/) {
+        // TODO: implémenter
+    }
+
+}
